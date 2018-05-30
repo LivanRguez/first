65957957d245b84c1e97ad87253c996fbcb7723b
LivanRguez <livanrguez@hotmail.com>
2018-05-30 14:05:15 -0600

    Es un hola mundo

---
+++ src/Principal.java
@@ -0,0 +1,15 @@
+
+import javax.swing.JOptionPane;
+
+
+public class Principal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        JOptionPane.showMessageDialog(null, "Hello Word");
+    }
+    
+}
